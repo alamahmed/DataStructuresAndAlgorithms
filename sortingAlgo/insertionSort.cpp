80eaa2bb24594fdfd167b95c207ccb5a9b6b0bc1
Ahmed Alam <ahmedalam@Ahmeds-MacBook-Pro.local>
2023-04-14 16:14:09 +0500

    Add Insertion Sort Using Recursion

--- sortingAlgo/insertionSort.cpp
+++ sortingAlgo/insertionSort.cpp
@@ -3,24 +3,58 @@
 
 void print( int *arr, int size );
 void insertionSort( int *arr, int size );
+void child( int *arr, int index );
+void parent( int *arr, int size, int index );
 
 int main()
 {
-    int size;
-    cout << "Enter the size of the array:\n";
-    cin >> size;
-    int arr[size];
+    int size = 5;
+    // cout << "Enter the size of the array:\n";
+    // cin >> size;
+    // int arr[size];
+    int arr[] = { 5, 4, 3, 2, 1 };
 
-    cout << "Enter values in the array:\n";
-    for( int i = 0; i < size; i++ )
-    {
-        cin >> arr[ i ];
-    }
+    // cout << "Enter values in the array:\n";
+    // for( int i = 0; i < size; i++ )
+    // {
+    //     cin >> arr[ i ];
+    // }
     cout << "\nArray before Sorting:\n";
     print( arr, size );
-    insertionSort( arr, size );
+    
+    parent( arr, size - 1, 0 );
+    cout << "\nArray After Sorting:\n";
+    print( arr, size );
+    // insertionSort( arr, size );
 
 }
+
+void child( int *arr, int index )
+{
+    if( index == 0 || arr[ index ] > arr[ index - 1 ] )
+    {
+        return;
+    }
+    else if( arr[ index ] < arr[ index - 1 ] )
+    {
+        int temp = arr[ index ];
+        arr[ index ] = arr[ index - 1 ];
+        arr[ index - 1 ] = temp;
+    }
+    child ( arr, --index );
+
+}
+
+void parent( int *arr, int size, int index )
+{
+    child( arr, index + 1 );
+    if ( index == size - 1 )
+    {
+        return;
+    }
+    parent( arr, size, ++index );
+}
+
 
 void insertionSort( int *arr, int size )
 {
